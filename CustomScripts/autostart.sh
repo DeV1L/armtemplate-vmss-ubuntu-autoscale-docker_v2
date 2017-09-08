a52bfc3c4e6bc1d45c6639b8cd16756467321f82
y.savchenko <y.savchenko@arkadium.com>
2017-09-08 11:24:47 +0300

    change container in script

--- CustomScripts/autostart.sh
+++ CustomScripts/autostart.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y docker-ce htop
 sudo mount -t cifs //$3.file.core.windows.net/www /mnt/ -o vers=3.0,username=$3,password=$4,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
 sudo docker login arkadiumcom.azurecr.io -u $1 -p $2
-sudo docker run -d --name arkadiumcom -p 80:80 -p 443:443 -v /mnt:/var/www arkadiumcom.azurecr.io/arkadiumcom/content-apache-ssl:latest
+sudo docker run -d --name arkadiumcom -p 80:80 -p 443:443 -v /mnt:/var/www arkadiumcom.azurecr.io/arkadiumcom/apache-ssl
