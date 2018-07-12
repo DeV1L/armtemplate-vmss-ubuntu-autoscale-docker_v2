6d5c443efca1a921f46052b46db5baa8953de49c
y.savchenko <y.savchenko@arkadium.com>
2018-07-12 17:19:04 +0300

    change webserver to NGINX with local cache

--- CustomScripts/autostart.sh
+++ CustomScripts/autostart.sh
@@ -4,7 +4,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update -y
 sudo apt-get install -y docker-ce htop
 
-sudo mount -t cifs //$3.file.core.windows.net/www /mnt/ -o vers=3.0,username=$3,password=$4,dir_mode=0777,file_mode=0777,sec=ntlmssp
+mkdir /mnt/html
+sudo mount -t cifs //$3.file.core.windows.net/www /mnt/html -o vers=3.0,username=$3,password=$4,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
 sudo docker login arkadiumcom.azurecr.io -u $1 -p $2
-sudo docker run -d --name arkadiumcom -p 80:80 -p 443:443 -v /mnt/html:/var/www/html arkadiumcom.azurecr.io/arkadiumcom/apache-ssl
+sudo docker run -d --name arkadiumcom -p 80:80 -p 443:443 -v /mnt/html:/usr/share/nginx/wordpress arkadiumcom.azurecr.io/arkadiumcom/arkadiumcom/nginx-ssl-cahce
